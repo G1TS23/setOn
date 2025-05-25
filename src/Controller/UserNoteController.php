ebeb2390570f2e7c21a1dd43fcb142647c700432
olivierfalahi <olivier.falahi@gmail.com>
2025-05-25 14:26:08 +0200

    :sparkles: Add real-time note updates with autosave and Mercure integration
    Implemented real-time synchronization for note titles and content using Mercure SSE. Introduced an autosave endpoint and a custom JWT provider for secure publishing of updates. Updated the frontend to support live updates and improved textarea resizing functionality.

--- src/Controller/UserNoteController.php
+++ src/Controller/UserNoteController.php
@@ -6,8 +6,12 @@ use App\Entity\Note;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/note')]
@@ -78,5 +82,54 @@ final class UserNoteController extends AbstractController
         }
 
         return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}/autosave', name: 'app_note_autosave', methods: ['POST'])]
+    public function autosave(Request $request, Note $note, EntityManagerInterface $entityManager, MessageBusInterface $bus, Security $security): JsonResponse
+    {
+        $user = $security->getUser();
+        if ($note->getOwner() !== $user && !$note->getEditors()->contains($user)) {
+            return new JsonResponse(['error' => 'You do not have permission to edit this note.'], Response::HTTP_FORBIDDEN);
+        }
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['field'], $data['value'])) {
+            $field = $data['field'];
+            $value = $data['value'];
+
+            if ($field === 'title') {
+                $note->setTitle($value);
+            } elseif ($field === 'content') {
+                $note->setContent($value);
+            } else {
+                return new JsonResponse(['error' => 'Invalid field'], Response::HTTP_BAD_REQUEST);
+            }
+
+            $entityManager->flush();
+
+            $noteUrl = '/notes/' . $note->getId();
+            $userNotesUrl = '/user/' . $note->getOwner()->getId() . '/notes/';
+
+            $updateNote = new Update($noteUrl, json_encode([
+                'id' => $note->getId(),
+                'content' => $note->getContent(),
+                'title' => $note->getTitle()
+            ]));
+            $updateUserNotes = new Update($userNotesUrl, json_encode([
+                ['id' => $note->getId(), 'title' => $note->getTitle()],
+                ['id' => $note->getId(), 'title' => $note->getTitle()]
+            ]));
+
+            try {
+                $bus->dispatch($updateNote);
+                $bus->dispatch($updateUserNotes);
+            } catch (ExceptionInterface $e) {
+                return new JsonResponse($e->getMessage(), Response::HTTP_BAD_REQUEST);
+            }
+
+            return new JsonResponse(['success' => true]);
+        }
+
+        return new JsonResponse(['error' => 'Invalid data'], Response::HTTP_BAD_REQUEST);
     }
 }
